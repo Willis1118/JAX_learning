10e457eda5a837a5587c946698f40e3aa449f11d
Willis1118 <nm3607@nyu.edu>
2023-05-22 18:48:54 -0400

    dependencies

---
+++ starter.py
@@ -0,0 +1,12 @@
+import jax.numpy as jnp
+import numpy as np
+
+# vital transform functions
+from jax import grad, jit, vmap, pmap
+
+from jax import lax
+
+from jax import make_jaxpr
+from jax import random
+from jax import device_put
+import matplotlib.pyplot as plt
