e83c121af2c774e2dc5fd4f57f74f610b56c3da7
Willis1118 <nm3607@nyu.edu>
2023-05-22 19:05:56 -0400

    check dep

--- starter.py
+++ starter.py
@@ -9,6 +9,6 @@ from jax import lax
 from jax import make_jaxpr
 from jax import random
 from jax import device_put
-import matplotlib.pyplot as plt
 
+from flax import linen as nn
 ### build first nn using JAX & Flax here
