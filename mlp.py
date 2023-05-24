b826c5a190db898c2b2fcd8e01147cef391e9aa6
Willis1118 <nm3607@nyu.edu>
2023-05-24 13:24:25 -0400

    cell

--- mlp.py
+++ mlp.py
@@ -40,7 +40,34 @@ def init_model(rng):
 def forward(params, xs):
     return params.weight * xs + params.bias
 
-# def loss_fn(params, )
+def loss_fn(params, xs, ys):
+    return jnp.mean((forward(params, xs) - ys)**2)
+
+@functools.partial(jax.pmap, axis_name='batch') # axis 0 by default
+def update(params, xs, ys):
+    '''
+        Backward propogration
+        input: distributed params, data, lable
+        output: updated params, loss
+    '''
+    loss, grads = jax.value_and_grad(loss_fn)(params, xs, ys)
+
+    # synchronize the gradient on all devices
+    grads = jax.lax.pmean(grads, axis_name='batch')
+
+    # Also the loss
+    loss = jax.lax.pmean(loss, axis_name='batch')
+
+    # now performing SGD on EACH device
+    new_params = jax.tree_map(
+        lambda param, g: param - g*lr, params, grads
+    )
+
+    # if using other optimizer, we could do
+    # updates, new_opt_state = optimizer(grads, opt_state)
+    # then use updates instead of grads to update the params
+
+    return new_params, loss
 
 '''
     Modeling data: y = w*x + b + eps
@@ -67,3 +94,25 @@ xp = reshape_for_pmap(xs, n_devices)
 yp = reshape_for_pmap(ys, n_devices)
 
 print(xp.shape, yp.shape)
+
+def type_after_update(name, obj):
+    print(f"after first `update()`, `{name}` is a {type(obj)}")
+
+num_epochs = 1000
+for epoch in range(num_epochs):
+
+    # where params and data gets communicated to devices
+    replicated_params, loss = update(replicated_params, xp, yp)
+
+    if epoch == 0:
+        type_after_update('replicated_params.weight', replicated_params.weight)
+        type_after_update('loss', loss)
+        type_after_update('xp', xp)
+    
+    if epoch % 100 == 0:
+        print(loss.shape)
+        print(f"Step {epoch:3d}, loss {loss[0]:3f}")
+
+# extract the params from distributed to a single device
+# device_get will extract data from TPU and fetch into host memory
+params = jax.device_get(jax.tree_map(lambda x: x[0], replicated_params))
