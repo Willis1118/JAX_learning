1dc989843f4bde350d47342c8f287270f3e7e557
Willis1118 <nm3607@nyu.edu>
2023-05-25 14:22:51 -0400

    cell

--- MNIST/train.py
+++ MNIST/train.py
@@ -1,13 +1,25 @@
 import numpy as np
 
+from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 
-def to_nparr(x):
+def to_flatten_nparr(x):
     '''
         transform PILImage to nparray
     '''
-    return np.array(x, dtype=np.float32)
+    return np.ravel(np.array(x, dtype=np.float32))
 
-train_dataset = MNIST(root='train_mnist', train=True, download=True, transform=to_nparr)
-test_dataset = MNIST(root='test_mnist', train=False, download=True, transform=to_nparr)
+def collate_fn(data):
+    print(data)
+
+batch_size = 128
+train_dataset = MNIST(root='train_mnist', train=True, download=True, transform=to_flatten_nparr)
+test_dataset = MNIST(root='test_mnist', train=False, download=True, transform=to_flatten_nparr)
+
+train_lodaer = DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=None,
+)
 
