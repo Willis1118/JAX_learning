cd553fd4413d0d6420449f3f57e2ba355557a903
Willis1118 <nm3607@nyu.edu>
2023-05-25 14:15:45 -0400

    cell

--- MNIST/train.py
+++ MNIST/train.py
@@ -1,4 +1,14 @@
+import numpy as np
+
 from torchvision.datasets import MNIST
 
-train_dataset = MNIST(root='train_mnist', train=True, download=True, transform=None)
-print(type(train_dataset))
+def to_nparr(x):
+    '''
+        transform PILImage to nparray
+    '''
+    return np.array(x, dtype=np.float32)
+
+train_dataset = MNIST(root='train_mnist', train=True, download=True, transform=to_nparr)
+data, label = train_dataset[0]
+print(data, label)
+
