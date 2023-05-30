10da0a8766c585f1d2108311b18e03bffe4d845b
Willis1118 <nm3607@nyu.edu>
2023-05-30 15:56:55 -0400

    init_param

--- Flax/main.py
+++ Flax/main.py
@@ -16,10 +16,25 @@ from typing import Any, Callable, Sequence, Optional
 import numpy as np
 # import matplotlib.pyplot as plt
 
+seed = 0
+
+def init_params(model):
+    key1, key2 = jax.random.split(jax.random.PRNGKey(seed))
+
+    ## init dummy input
+    x = jax.random.normal(key1, (10,))
+
+    ## init call; remember jax handles state externally
+    y, params = model.init_with_output(key2, x)
+
+    print(y)
+    print((jax.tree_map(lambda x: x.shape, params)))
+
 if __name__ == '__main__':
     ## single forward-feed layer
     model = nn.Dense(features=5)
 
     ## All Flax NN layer inherit from the Module class
     print(nn.Dense.__base__)
+
 
