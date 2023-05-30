841138a7503ce3f4568d5c797940db932b78384c
Willis1118 <nm3607@nyu.edu>
2023-05-30 16:21:46 -0400

    data

--- Flax/main.py
+++ Flax/main.py
@@ -55,6 +55,29 @@ if __name__ == '__main__':
     try:
         y = model(x)
     except Exception as e:
-        print(e)
+        print(e) # --> unbound module
+
+    ##### Simple Linear Reg
+
+    n_samples = 1000
+    x_dim = 2
+    y_dim = 1
+    noise_amp = 0.1
+
+    ## Generate gt W & b
+    key, w_k, b_k = jax.random.split(jax.random.PRNGKey(seed), num=3)
+    W = jax.random.normal(w_k, (x_dim, y_dim))
+    b = jax.random.normal(b_k, (y_dim,))
+
+    true_params = freeze({'params': {'bias': b, 'kernel': W}})
+
+    ## Generate Data
+    key, x_k, eps_k = jax.random.split(key, num=3)
+    xs = jax.random.normal(x_k, (n_samples, x_dim))
+    ys = jnp.dot(xs, W) + b
+    ys += noise_amp * jax.random.normal(eps_k, (n_samples, y_dim))
+
+    print(f"input shape: {xs.shape}, target shape: {ys.shape}")
+ 
 
 
