499eafc0e3d0ca646eac798f54f86947783de2fa
Willis1118 <nm3607@nyu.edu>
2023-05-31 12:01:56 -0400

    DDPM

---
+++ DDPM/helpers.py
@@ -0,0 +1,65 @@
+'''
+    Helper functions for implementing the U-Net
+'''
+
+import flax
+from flax import linen as nn
+
+## import python built-in functions
+from inspect import isfunction
+from functools import partial
+from typing import Any, Callable, Sequence, Optional
+
+def exists(x):
+    '''
+        check if x is defined
+    '''
+    return x is not None
+
+def default(val, d):
+    '''
+        return val if exists else d
+    '''
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+class Residual(nn.Module):
+    fn: Callable # anything with __call__ method
+
+    def __call__(self, x, *args, **kwargs):
+        '''
+            define the res connection
+        '''
+        return self.fn(x, *args, **kwargs) + x
+
+def Upsample(dim):
+
+    '''
+        input: (dim, H, W)
+        output: 
+            (dim,
+             (H-1) * stride[0] - 2 * padding[0] + dilation[0] * (kernel_size[0] - 1) + output_padding[0] + 1),
+             (W-1) * stride[1] - 2 * padding[1] + dilation[1] * (kernel_size[1] - 1) + output_padding[1] + 1),
+            )
+    '''
+
+    return nn.ConvTranspose(
+        features=dim,
+        kernel_size=4,
+        strides=2,
+        padding=1
+    )
+
+def Downsample(dim):
+    return nn.Conv(
+        features=dim,
+        kernel_size=4,
+        strides=2,
+        padding=1
+    )
+
+
+
+
+
---
+++ DDPM/model.py
@@ -0,0 +1,255 @@
+'''
+    Defining model block
+'''
+
+import jax
+from jax import lax, random, numpy as jnp
+
+# NN lib built on top of JAX developed
+import flax
+from flax.core import freeze, unfreeze
+from flax import linen as nn
+from flax.training import train_state
+
+import optax
+
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+
+from einops import rearrange
+
+## python importing
+import math
+from typing import Callable, Tuple
+from functools import partial
+
+## custom module importing
+from helpers import exists, default, Residual, Downsample, Upsample
+
+### Time Positional Embedding ###
+class PositionalEmbedding(nn.Module):
+    dim: int
+
+    def __call__(self, time):
+        '''
+            sin pos embedding
+            input: (B, 1) time tensor
+            output: (B, dim) embeddings
+        '''
+
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = jnp.exp(jnp.arange(half_dim) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        # embeddings = jnp.inner(time, embeddings)
+        embeddings = jnp.concatenate((embeddings.sin(), embeddings.cos()), axis=-1)
+        return embeddings
+
+class Block(nn.Module):
+    dim: int
+    groups: int
+
+    '''
+        A Basic GroupNorm Block
+    '''
+
+    @nn.compact
+    def __call__(self, x, *, scale_shift=None):
+        x = nn.Conv(
+            features=self.dim,
+            kernel_size=3,
+            padding=1,
+        )(x)
+        x = nn.GroupNorm(
+            num_groups=self.groups,
+        )(x)
+
+        if exists(scale_shift):
+            scale, shift = scale_shift
+            x = x * (scale + 1) + shift
+        
+        return nn.activation.silu(x)
+
+class ResNetBlock(nn.Module):
+    dim_out: int
+    emb_dim: int
+    groups: int
+
+    '''
+        ResNet Block
+    '''
+    @nn.compact
+    def __call__(self, x, time_emb=None):
+        h = Block(self.dim_out, self.groups)(x)
+
+        if exists(time_emb):
+            time_emb = nn.Dense(features=self.dim_out)(nn.activation.silu(time_emb))
+            h = rearrange(time_emb, 'b c -> b c 1 1') + h
+        
+        h = Block(self.dim_out, self.groups)(x)
+        
+        if x.shape[1] != h.shape[1]:
+            h += nn.Conv(self.dim_out, 1)(x)
+        else:
+            h += x
+        
+        return h
+
+class Attention(nn.Module):
+    dim: int
+    heads: int
+    dim_head: int
+
+    '''
+        Attention Module
+    '''
+    @nn.compact
+    def __call__(self, x):
+        b, c, h, w = x.shape
+        hidden = self.heads * self.dim_heads
+        scale = self.dim_head ** -0.5
+        qkv = nn.Conv(
+            features=hidden * 3,
+            kernel_size=1,
+            use_bias=False
+        )(x).split(3, axis=1)
+
+        '''
+            Attention process
+        '''
+
+        q, k, v = map(
+            lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h=self.heads), qkv
+        )
+
+        q = q * scale
+        esum = jnp.einsum('b h d i, b h d j -> b h i j', q, k)
+        esum -= esum.amax(axis=-1, keepdims=True)
+        attn = esum.softmax(axis=-1)
+
+        out = jnp.einsum('b h i j, b h d j -> b h i d', attn, v)
+        out = rearrange('b h (x y) d -> b (h d) x y', x=h, y=w)
+        return nn.Conv(self.dim)(out)
+    
+class LinearAttention(nn.Module):
+    dim: int
+    heads: int
+    dim_head: int
+
+    @nn.compact
+    def __call__(self, x):
+        b, c, h, w = x.shape
+        hidden = self.heads * self.dim_heads
+        scale = self.dim_head ** -0.5
+        qkv = nn.Conv(
+            features=hidden * 3,
+            kernel_size=1,
+            use_bias=False
+        )(x).split(3, axis=1)
+
+        '''
+            Attention process
+        '''
+
+        q, k, v = map(
+            lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h=self.heads), qkv
+        )
+
+        q = q.softmax(axis=-2)
+        k = k.softmax(axis=-1)
+
+        q = q * scale
+
+        context = jnp.einsum('b h d n, b h e n -> b h d e', k, v)
+
+        out = jnp.einsum('b h d e, b h d n -> b h e n', context, q)
+        out = rearrange('b h c (x y) -> b (h c) x y', h=self.heads, x=h, y=w)
+
+        return nn.Conv(self.dim, 1)(nn.GroupNorm(1)(out))
+    
+class PreNorm(nn.Module):
+    fn: Callable
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.GroupNorm(1)(x)
+        return self.fn(x)
+
+class UNet(nn.Module):
+    init_dim: int = None
+    out_dim: int = None
+    dim_mults: Tuple[int] = (1,2,4,8)
+    channels: int = 3
+    with_time_emb: bool = True
+    resnet_block_groups: int = 8
+    
+    @nn.compact
+    def __call__(self, x, time):
+        b, c, h, w = x.shape
+
+        init_dim = default(self.init_dim, c // 3 * 2)
+        init_conv = nn.Conv(init_dim, 7, padding=3)
+
+        dims = [init_dim, *map(lambda m: c * m, self.dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        block_class = partial(ResNetBlock, groups=self.resnet_block_groups)
+
+        if self.with_time_emb:
+            time_dim = c * 4
+            time_mlp = nn.Sequential(
+                PositionalEmbedding(c),
+                nn.Dense(time_dim),
+                nn.activation.gelu(),
+                nn.Dense(time_dim)
+            )
+        else:
+            time_mlp = None
+
+        x = init_conv(x)
+        t = time_mlp(time)
+        h = []
+
+        for i, (dim_in, dim_out) in enumerate(in_out):
+            is_last = i >= len(in_out) - 1
+
+            x = block_class(dim_out=dim_out, emb_dim=time_dim)(x, t)
+            x = block_class(dim_out=dim_out, emb_dim=time_dim)(x, t)
+
+            x = Residual(PreNorm(LinearAttention(dim_out).apply).apply).apply(x)
+
+            h.append(x)
+
+            if not is_last:
+                x = Downsample(dim_out)(x)
+            
+        mid_dim = dims[-1]
+        x = block_class(mid_dim, time_dim)(x, t)
+        x = Residual(PreNorm(LinearAttention(self.mid_dim).apply).apply).apply(x)
+        x = block_class(mid_dim, time_dim)(x, t)
+
+        for i, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            x = jnp.concatenate((x, h.pop()), axis=1)
+
+            is_last = i >= len(in_out) - 1
+
+            x = block_class(dim_out=dim_in, emb_dim=time_dim)(x, t)
+            x = block_class(dim_out=dim_in, emb_dim=time_dim)(x, t)
+
+            x = Residual(PreNorm(LinearAttention(dim_in).apply).apply).apply(x)
+
+            if not is_last:
+                x = Upsample(dim_in)(x)
+    
+        out_dim = default(self.out_dim, self.channels)
+        x = block_class(c)(x, t)
+        x = nn.Conv(out_dim, 1)(x)
+
+        return x
+
+
+
+
+        
+
+
