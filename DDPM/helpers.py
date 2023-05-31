8c321a69c4e7e1c7e882cd7973bdd3f0898bfddd
Willis1118 <nm3607@nyu.edu>
2023-05-31 12:52:11 -0400

    convT

--- DDPM/helpers.py
+++ DDPM/helpers.py
@@ -47,7 +47,7 @@ def Upsample(dim):
     return nn.ConvTranspose(
         features=dim,
         kernel_size=(4,4),
-        strides=2,
+        strides=[2],
         padding=1
     )
 
@@ -55,7 +55,7 @@ def Downsample(dim):
     return nn.Conv(
         features=dim,
         kernel_size=(4,4),
-        strides=[2],
+        strides=2,
         padding=1
     )
 
