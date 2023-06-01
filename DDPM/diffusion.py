adf1aca3ebfefc734fa91f908d31fff7eceea153
Willis1118 <nm3607@nyu.edu>
2023-06-01 11:50:46 -0400

    positional_arg

--- DDPM/diffusion.py
+++ DDPM/diffusion.py
@@ -63,7 +63,7 @@ class Diffuser:
     def extract(a, t, x_shape):
         batch = x_shape[0]
         print(t)
-        out = np.take_along_axis(jax.device_get(a), -1, np.array(t))
+        out = np.take_along_axis(jax.device_get(a), np.array(t), axis=-1)
         return jnp.array(np.reshape(out, (batch, *((1,) * (len(x_shape) - 1)))))
     
     # @partial(jax.jit, static_argnums=(4,))
