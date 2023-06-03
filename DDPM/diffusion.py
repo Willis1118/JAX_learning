8552508af6820ae43bdf004a1e3ced0024e9e909
Willis1118 <nm3607@nyu.edu>
2023-06-03 16:53:06 -0400

    fix model

--- DDPM/diffusion.py
+++ DDPM/diffusion.py
@@ -149,9 +149,9 @@ class Diffuser:
         for i in tqdm(reversed(range(0, self.time)), desc='sampling loop time step', total=self.time):
             key, sample_key = random.split(key)
             sample_key = jax_utils.replicate(sample_key)
-            t_index = jax_utils.replicate(i)
+            i
             
-            img = pp_sample(sample_key, params, img, jnp.full((n,b), i, dtype=jnp.int32), t_index)
+            img = pp_sample(sample_key, params, img, jnp.full((n,b), i, dtype=jnp.int32), i)
             imgs.append(jax.device_get(img))
         
         return imgs
